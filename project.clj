b688e4a87a79d10ae6e26c6944be8958d3bc8051
Devin Walters <devinw@gmail.com>
2012-02-18 21:28:12 -0600

    Initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject flurg "0.0.5"
+  :description "a project that is awesome"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [overtone "0.7.0-SNAPSHOT"]])
---
+++ test/flurg/test/core.clj
@@ -0,0 +1,6 @@
+(ns flurg.test.core
+  (:use [flurg.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
